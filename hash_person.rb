8357817bebe0945307859ee05ca84e2cf593c24a
John Leavell <johnleavell@gmail.com>
2023-03-23 15:28:52 -0500

    finsihged hash_person

--- hash_person.rb
+++ hash_person.rb
@@ -2,3 +2,15 @@
 # Make sure the value of the :age key is an Integer
 
 p "Enter a name, age, and occupation separated by spaces:"
+person = {}
+
+input = gets.chomp.split(" ")
+
+#iterate over the input
+input.each do |key|
+  #push attributes to the array
+  person.store(:first_name, input[0])
+  person.store(:age, input[1].to_i)
+  person.store(:occupation, input[2])
+  puts person
+end
